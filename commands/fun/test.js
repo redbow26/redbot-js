67338dc47d689938603d2e48bc604bd612d55217
Redbow <tristanleroy26@gmail.com>
2020-06-02 00:06:29 +0200

    VERSION 0.0.2, command Handler, init readme

---
+++ commands/fun/test.js
@@ -0,0 +1,3 @@
+module.exports.run = async(client, message, args) => {
+    message.channel.send("this is a test.")
+}
--- index.js
+++ index.js
@@ -1,21 +1,53 @@
 require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const fs = require('fs').promises;
+const path = require('path');
+
 const PREFIX = process.env.PREFIX;
+
+client.login(process.env.BOT_TOKEN);
+
+client.commands = new Map();
 
 client.on('ready', () => {
     console.log('Bot ready')
 });
 
-client.login(process.env.BOT_TOKEN).then(r => console.log(r))
 
-const isValidCommand = (message, cmdName) => message.content.toLowerCase().startsWith(PREFIX + cmdName)
+const isValidCommand = (message, cmdName) => message.content.toLowerCase().startsWith(PREFIX + cmdName);
 
 
 client.on('message', message => {
-
     if (message.author.bot) return;
-    if (isValidCommand(message, "test"))
-        message.channel.send("this is a test.")
 
-})
+    if (!message.content.startsWith(PREFIX)) return;
+    let cmdArgs = message.content.substring(message.content.indexOf(PREFIX) + 1).split(new RegExp(/\s+/));
+    let cmdName = cmdArgs.shift();
+
+    if (client.commands.get(cmdName)) {
+        client.commands.get(cmdName).run(client, message, cmdArgs);
+    } else {
+        console.log(cmdName + " does not exist.")
+    }
+
+});
+
+(async function registerCommands(dir = 'commands') {
+    // Read the directory/file
+    let files = await fs.readdir(path.join(__dirname, dir));
+    // Loop through each file
+    for (let file of files) {
+        let stat = await fs.lstat(path.join(__dirname, dir, file));
+        if (stat.isDirectory()) // If file is a directory, recursive call 
+            registerCommands(path.join(dir, file));
+        else{
+            // Check if file is a .js file
+            if (file.endsWith('.js')) {
+                let cmdName = file.substring(0, file.indexOf(".js"));
+                let cmdModule = require(path.join(__dirname, dir, file));
+                client.commands.set(cmdName, cmdModule);
+            }
+        }
+    }
+})();
