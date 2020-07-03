af94c0d92cab5434df6ee47fda183b8578ebbac3
Redbow <tristanleroy26@gmail.com>
2020-07-04 01:39:31 +0200

    VERSION 0.0.5, missing_arg event fix (send message and delete it)

--- src/events/error/missing_arg.js
+++ src/events/error/missing_arg.js
@@ -1,6 +1,6 @@
 // Custom Event
 
 module.exports = async (client, message) => {
-    msg = message.reply('Command need argument')
+    let msg = await message.channel.send('Command need argument');
     await msg.delete({ timeout: 3500 }).catch(err => console.log(err));
 }
